7c032cbd3f5b83d7124790f44d284e745e638165
shakil232 <dxshakil2@gmail.com>
2022-06-25 23:09:09 +0600

    try to deploy

--- index.js
+++ index.js
@@ -209,7 +209,10 @@ const run = async () => {
 // run-function-calling 
 run().catch(console.dir);
 
-
+// default-api 
+app.get('/', (req, res) => {
+    res.send('Welcome Modern Dental Clinic')
+})
 
 
 // LISTEN-PORT 
