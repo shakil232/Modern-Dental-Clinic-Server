75003d2e23022a9cb4f6f1c5b02ada0e5e30899d
shakil232 <dxshakil2@gmail.com>
2022-06-25 23:28:34 +0600

    try

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const admin = require("firebase-admin");
-const cors = require('cors');
 require('dotenv').config();
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 5001;
 
 
@@ -209,7 +209,10 @@ const run = async () => {
 // run-function-calling 
 run().catch(console.dir);
 
-
+// default-api 
+app.get('/', (req, res) => {
+    res.send('Welcome Modern Dental Clinic')
+})
 
 
 // LISTEN-PORT 
